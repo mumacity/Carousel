c59ecb8cf8019292cfc574ac6421fecf57d74f8d
mumacity <724724114@qq.com>
2018-09-28 12:32:37 +0800

    carousel

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        animateOut: 'lightSpeedOut',
+        items:1,
+        margin:30,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+    });
+  });
